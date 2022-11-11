61cc5c5a6792de79be8cf926edd9cf2d2ba96b52
Tyler Bedard <tylerbedard126@gmail.com>
2022-11-10 21:08:36 -0500

    added things to add tracks to page

--- deezerAPI.js
+++ deezerAPI.js
@@ -7,6 +7,7 @@ const artistName = document.getElementById('artistName')
 const albumNameEL = document.getElementById('albumName')
 // const albumPicUrl = img[`src`]
 const nextButton = document.createElement(`button`)
+const searchResults = document.getElementById(`searchResults`)
 
 
 
@@ -30,12 +31,21 @@ function getTopSongs(id) {
             return response.json()
         })
         .then(function (data) {
+            // const songNameEL = document.createElement(`h1`)
+            // const artistName = document.createElement(`p`)
+            // const albumNameEL = document.createElement(`p`)
             // img[`src`] = data.data[0].album.cover_medium
+            // for (let i = 0; i > 5; i++) {
             songNameEL.textContent = `Song Title:` + data.data[0].title
             artistName.textContent = `Artist Name:` + data.data[0].artist.name
             albumNameEL.textContent = `Album Name:` + data.data[0].album.title
-            // albumPicUrl.textContent = data.data[0].album.cover_medium
+            // document.body.appendChild(songNameEL)
+            // document.body.appendChild(artistName)
+            // document.body.appendChild(albumNameEL)
 
+            // songNameEL.css(`color`, `red`)
+            // albumPicUrl.textContent = data.data[0].album.cover_medium
+            // }
             // songNameEL.textContent = data.data[1].title + data.data[1].album.title + data.data[1].artist.name
             // songNameEL.textContent = data.data[2].title + data.data[2].album.title + data.data[2].artist.name
             // songNameEL.textContent = data.data[3].title + data.data[3].album.title + data.data[3].artist.name
@@ -44,7 +54,7 @@ function getTopSongs(id) {
             // nextButton.append(songNameEL)
             // albumPic.innerHTML = img[`src`]
             console.log(data.data[0].album.cover_medium)
-            console.log(data)
+            console.log(data.data[0])
         })
 }
 
