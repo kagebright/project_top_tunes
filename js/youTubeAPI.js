5c65cd8e92782663defc5d3b5325fef9ff76fa3b
kagebright <113262558+kagebright@users.noreply.github.com>
2023-03-06 20:03:43 -0500

    Update youTubeAPI.js

--- js/youTubeAPI.js
+++ js/youTubeAPI.js
@@ -1,33 +1,21 @@
+const apiKey = "AIzaSyDyBN4PZvhznxRfK4dKAvNGFHMFS8yuaq4";
 
-const apiKey= "AIzaSyDyBN4PZvhznxRfK4dKAvNGFHMFS8yuaq4"
-const videosEL= document.getElementById("videos");
+const videosEL = document.getElementById("videos");
 
-
-// "AIzaSyCvwFXwykEYMZpivD7c0pXm2KjCi1srHwY"
-
-function youtubeApi(searchArtist) {
-    const apiKey = "AIzaSyDyBN4PZvhznxRfK4dKAvNGFHMFS8yuaq4"
-    const queryURL = "https://www.googleapis.com/youtube/v3/search?q=" + searchArtist + "&key="+ apiKey;
-    
-    fetch(queryURL)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function(data) {
-          console.log(data)
-        const videoId = data.items[0].id.videoId;
-        const queryURL = "https://www.googleapis.com/youtube/v3/videos?part=player&id=" + videoId + "&key=" + apiKey;
-        
-        fetch(queryURL)
-        .then(function (response) {
-            return response.json();
-          })
-        
-          .then(function (data) {
-            console.log(data)
-            videosEL.innerHTML = data.items[0].player.embedHtml;
-          
-          });
-        });
+async function youtubeApi(searchArtist) {
+  
+    try {
+    const queryURL = `https://www.googleapis.com/youtube/v3/search?q=${searchArtist}&key=${apiKey}`;
+    const response = await fetch(queryURL);
+    const data = await response.json();
+    const videoId = data.items[0].id.videoId;
+    const videoQueryURL = `https://www.googleapis.com/youtube/v3/videos?part=player&id=${videoId}&key=${apiKey}`;
+    const videoResponse = await fetch(videoQueryURL);
+    const videoData = await videoResponse.json();
+    videosEL.innerHTML = videoData.items[0].player.embedHtml;
+      
+  } catch (error) {
+    console.error(error);
+  }
 }
 
